e8c73e5b944a4720f049bde55d4a2e7f45334800
David Villalobos <davidevm@Davids-MacBook-Pro.local>
2018-10-19 13:37:46 -0600

    icon

--- src/app/models/usuario.ts
+++ src/app/models/usuario.ts
@@ -2,7 +2,7 @@ export class Usuario {
 	public _id: string = undefined;
 	public usuario : string = '';
 	public nombre : string = '';
-	public apellido : string = '';
+	public apellido1 : string = '';
 	public correo : string = '';
 	public passw : string = '';
 	constructor(){
